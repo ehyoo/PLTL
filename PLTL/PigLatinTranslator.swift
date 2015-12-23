b45f541ee1037ab0cbea236cc6ebeff3e8288766
Edward Yoo <ehyoo97@gmail.com>
2015-12-22 23:03:55 -0500

    updates translator- adds punctuation conservation.

--- PLTL/PigLatinTranslator.swift
+++ PLTL/PigLatinTranslator.swift
@@ -10,7 +10,68 @@ import Foundation
 
 class Translator {
   
+  let punctuationArray = []
+  
+  //split everything into chars
+  //iterate through it:
+  /*
+  if x != letter or number {
+  join beginning -> x to the index before the punctuation
+  add to new array
+  pop everything there
+  } else {
+  add to new array
+  pop
+  }
+  
+  join entire array and return.
+  */
+  
   func toPigLatin(stringToTranslate: String) -> String {
+    
+    var stringArray = stringToTranslate.characters.map { String($0) }
+    var translatedArray: [String] = []
+    let alphanumericCharacterSet = NSCharacterSet.alphanumericCharacterSet()
+    var finalString = ""
+    var lastKnownIndex = 0
+    
+    for i in 0..<stringArray.count {
+      if let _ = stringArray[i].rangeOfCharacterFromSet(alphanumericCharacterSet) {
+        
+        if i == stringArray.count - 1 {
+          
+          let isolatedWordArray = stringArray[lastKnownIndex..<stringArray.count]
+          let isolatedWord = isolatedWordArray.joinWithSeparator("")
+          let pigLatinizedWord = pigLatinTranslator(isolatedWord)
+          translatedArray.append(pigLatinizedWord)
+        }
+      } else {
+        
+        //takes all the characters up to the special character and combines them into an array
+        //then adds to the translatedArray
+        if i != lastKnownIndex + 1 {
+          //if not the first character
+          let isolatedWordArray = stringArray[lastKnownIndex..<i]
+          let isolatedWord = isolatedWordArray.joinWithSeparator("")
+          let pigLatinizedWord = pigLatinTranslator(isolatedWord)
+          translatedArray.append(pigLatinizedWord)
+          lastKnownIndex = i
+          
+          //add special character
+          translatedArray.append(stringArray[i])
+        } else {
+          translatedArray.append(stringArray[i])
+          lastKnownIndex = i
+        }
+      }
+    }
+    
+    finalString = translatedArray.joinWithSeparator("")
+    
+    return finalString
+  }
+  
+  func pigLatinTranslator(stringToTranslate: String) -> String {
     
     let vowelArray = ["a", "e", "i", "o", "u"]
     let stringToTranslateArray = stringToTranslate.characters.split{$0 == " "}.map(String.init)
